987d75fa134d5d18902a2193110680829e047a9a
jean <jean.ma.1986@gmail.com>
2015-09-03 16:29:40 +0800

    update website

--- src/ExamEngineService/Website/js/app.config.js
+++ src/ExamEngineService/Website/js/app.config.js
@@ -1,4 +1,4 @@
-/*!
+﻿/*!
  * Created by Jean on 8/29/2015.
  * 
  * email:mahai_1986@126.com
@@ -13,8 +13,8 @@ define({
         url: "/"
         , templateUrl: "default.html"
     }, {
-        url: "/forms"
-        , templateUrl: "partial_container.html"
+        url: "/task/:id"
+        , templateUrl: "task.html"
     }, {
         url: "/test"
         , templateUrl: "test.html"
@@ -23,6 +23,16 @@ define({
         , templateUrl: "login.html"
     }]
     , formDependence: {
-        "forms/test-frm1.html": ["table-form"]
+        "task_id": {
+            deps: []//angular dependence model
+            , path: ""//form path
+        }
+        , "test_frm1": {
+            deps: ["table-form"]
+            , path: "forms/test-frm1.html"
+        }
+        , "医疗保险费单位缴费月报表": {
+            path: "forms/医疗保险费单位缴费月报表.html"
+        }
     }
 });
