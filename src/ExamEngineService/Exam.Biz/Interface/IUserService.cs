e638b39e312168850dfe1a71c10234e447319099
=youkaisteve <=youkaisteve@gmail.com>
2015-09-06 23:57:22 +0800

    aa

--- src/ExamEngineService/Exam.Biz/Interface/IUserService.cs
+++ src/ExamEngineService/Exam.Biz/Interface/IUserService.cs
@@ -9,6 +9,17 @@ namespace Exam.Service.Interface
 {
     public interface IUserService
     {
+        /// <summary>
+        /// 根据小组sysno获取用户
+        /// </summary>
+        /// <param name="sysNo">小组sysno</param>
+        /// <returns></returns>
         List<dynamic> GetUserByTeamSysNo(int sysNo);
+
+        /// <summary>
+        /// 获取所有小组，包括用户信息
+        /// </summary>
+        /// <returns></returns>
+        List<dynamic> GetAllTeamsWithUser();
     }
 }
