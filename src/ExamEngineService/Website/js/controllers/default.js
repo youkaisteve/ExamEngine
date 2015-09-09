e65ea09fa6239192e941744074ef13e3579ef187
jean <jean.ma.1986@gmail.com>
2015-09-09 23:07:37 +0800

    merge

--- src/ExamEngineService/Website/js/controllers/default.js
+++ src/ExamEngineService/Website/js/controllers/default.js
@@ -6,31 +6,36 @@
  */
 define(["app"], function (app) {
 
-    app.controller("default", ["$scope","$window", function ($scope,$window) {
+    app.controller("default", ["$scope", "$window", function ($scope, $window) {
 
         //TODO location target page by role
 
 
-        //$scope.path = "partials/default_student.html";
-        $scope.path = "partials/default_teacher.html";
+        $scope.path = "partials/default_student.html";
+        //$scope.path = "partials/default_teacher.html";
         $scope.fixedFooter = true;
 
         $scope.tasks = [];
 
-        $scope._request("Tasks", {
-            UserId: "007"
-            , PageInfo: {
-                PageIndex: 0
-                , PageSize: 100
-            }
-        }).success(function (res) {
-            if(res.Code===0) {
-                $scope.tasks =res.Data.Tasks;
-            }
-            else{
-                $window.alert(res.ErrorMessage);
-            }
-        });
+
+        $scope.getTasks = function () {
+            $scope._request("Tasks", {
+                UserId: "007"
+                //, PageInfo: {
+                //    PageIndex: 0
+                //    , PageSize: 100
+                //}
+            }).success(function (res) {
+                if (res.Code === 0) {
+                    $scope.tasks = res.Data.Tasks;
+                }
+                else {
+                    $window.alert(res.ErrorMessage);
+                }
+            });
+        };
+
+        $scope.getTasks();
 
     }]);
 
