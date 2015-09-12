ce1d262a7f95f231f5ba379183bafabaf05ddeec
jean <jean.ma.1986@gmail.com>
2015-09-12 13:08:02 +0800

    merge

--- src/ExamEngineService/Website/js/controllers/login.js
+++ src/ExamEngineService/Website/js/controllers/login.js
@@ -4,22 +4,22 @@
  * email:mahai_1986@126.com
  *
  */
-define(["app"], function (app) {
+define(["app","disabled-when-click"], function (app) {
 
     app.controller("login", ["$scope", "$window", function ($scope, $window) {
 
-        $scope.logining=false;
+        //$scope.logining=false;
         $scope.login = function ($event, model) {
-            $scope.logining=true;
+            //$scope.logining=true;
             return $scope._request("Login", model).then(function (res) {
                 //location to default page when login success
                 //TODO save auth info when login success
                 //auth info contain Role,UserId
                 $scope._auth(res.Data);
                 $scope._goto("/default");
-            }).finally(function(){
+            })/*.finally(function(){
                 $scope.logining=false;
-            });
+            })*/;
         }
     }]);
 
