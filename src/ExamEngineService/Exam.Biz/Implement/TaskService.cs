7dcdbaf2249375a27f231c4e073c8e6d29ac54fb
kai.you <kai.you@fangyibao.com>
2015-09-14 15:55:01 +0800

    fix bug

--- src/ExamEngineService/Exam.Biz/Implement/TaskService.cs
+++ src/ExamEngineService/Exam.Biz/Implement/TaskService.cs
@@ -140,26 +140,31 @@ namespace Exam.Service.Implement
             }
 
             //获取下一个节点名并启动流程
-            string nodeName = proxy.GetTransitionNextNodeRoles(data.DefineName, data.TokenName, data.TransitionName)[0];
-            List<User> users = teamRepo.GetUsersByNodeName(data.DefineName, nodeName);
-            User choosenUser = GetRandomUserId(users);
-            if (choosenUser == null)
+            var list = proxy.GetTransitionNextNodeRoles(data.DefineName, data.TokenName, data.TransitionName);
+            if (list != null && list.Count > 0)
             {
-                throw new BusinessException("找不到下一步处理人");
+
+                string nodeName = list[0];
+                List<User> users = teamRepo.GetUsersByNodeName(data.DefineName, nodeName);
+                User choosenUser = GetRandomUserId(users);
+                if (choosenUser == null)
+                {
+                    throw new BusinessException("找不到下一步处理人");
+                }
+
+                assignedUserRepo.Insert(new AssignedUser
+                {
+                    InDate = DateTime.Now,
+                    InstanceID = data.InstanceId,
+                    UserID = choosenUser.UserID,
+                    Nodename = nodeName,
+                    ProcessName = data.DefineName,
+                    InUser = data.User.UserID
+                });
+
+                var user = new TaskUser { UserId = choosenUser.UserID, UserName = choosenUser.UserName };
+                processInstance.IncludeActors.Add(user);
             }
-
-            assignedUserRepo.Insert(new AssignedUser
-            {
-                InDate = DateTime.Now,
-                InstanceID = data.InstanceId,
-                UserID = choosenUser.UserID,
-                Nodename = nodeName,
-                ProcessName = data.DefineName,
-                InUser = data.User.UserID
-            });
-
-            var user = new TaskUser { UserId = choosenUser.UserID, UserName = choosenUser.UserName };
-            processInstance.IncludeActors.Add(user);
 
             proxy.ProcessExecuter(processInstance);
 
