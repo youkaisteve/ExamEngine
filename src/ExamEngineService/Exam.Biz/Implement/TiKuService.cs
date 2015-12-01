cbe2ca9704743d0246991e1365876e1814945eb7
kai.you <kai.you@fangyibao.com>
2015-12-01 11:50:33 +0800

    add

--- src/ExamEngineService/Exam.Biz/Implement/TiKuService.cs
+++ src/ExamEngineService/Exam.Biz/Implement/TiKuService.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exam.Service.Interface
 {
-    public class TiKuService
+    [Export(typeof(ITiKuService))]
+    public class TiKuService : ServiceBase, ITiKuService
     {
+        protected override string ModuleName
+        {
+            get { return "TiKu"; }
+        }
+
+        public void CreateOrUpdateProcessInfo(Repository.ProcessInfo pInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Repository.ProcessInfo GetProcessInfo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Repository.ProcessInfo GetProcessInfo(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Repository.ProcessInfo> GetAllProcess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Repository.TiKuMaster> GetAllTiKu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateOrUpdateTiKu(Repository.TiKuMaster master)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/ExamEngineService/Exam.Biz/Interface/ITiKuService.cs
+++ src/ExamEngineService/Exam.Biz/Interface/ITiKuService.cs
@@ -1,4 +1,4 @@
-﻿using Exam.Model;
+﻿using Exam.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +9,15 @@ namespace Exam.Service.Interface
 {
     public interface ITiKuService
     {
-        void CreateTiKu(TiKuMaster master);
-        void updateTiKU(TiKuMaster master);
+        void CreateOrUpdateProcessInfo(ProcessInfo pInfo);
+
+        ProcessInfo GetProcessInfo(int id);
+        ProcessInfo GetProcessInfo(string name);
+
+        List<ProcessInfo> GetAllProcess();
+
+
+        List<TiKuMaster> GetAllTiKu();
+        void CreateOrUpdateTiKu(TiKuMaster master);
     }
 }
