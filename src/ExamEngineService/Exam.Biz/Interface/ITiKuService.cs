2d0d43c5ddb946ebe975693daaed36248c664ec6
kai.you <kai.you@fangyibao.com>
2015-12-01 11:34:46 +0800

    add

---
+++ src/ExamEngineService/Exam.Biz/Interface/ITiKuService.cs
@@ -0,0 +1,15 @@
+﻿using Exam.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Service.Interface
+{
+    public interface ITiKuService
+    {
+        void CreateTiKu(TiKuMaster master);
+        void updateTiKU(TiKuMaster master);
+    }
+}
---
+++ src/ExamEngineService/Exam.Model/TiKu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Model
+{
+    public class TiKuMaster : ModelBase
+    {
+        public int SysNo { get; set; }
+        public string TiKuName { get; set; }
+        public string InUser { get; set; }
+        public DateTime InDate { get; set; }
+        public string LastEditUser { get; set; }
+        public DateTime? LastEditDate { get; set; }
+        public int Status { get; set; }
+    }
+
+    public class TiKuDetail
+    {
+        public int SysNo { get; set; }
+        public int MasterSysNo { get; set; }
+        public int ProcessInfoSysNo { get; set; }
+        public string NodeName { get; set; }
+        public string TeamName { get; set; }
+    }
+}
