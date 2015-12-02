a3d9c1c5977f8bc0c349459791591076dbdd3241
kai.you <kai.you@fangyibao.com>
2015-12-02 15:59:37 +0800

    fix

--- src/ExamEngineService/Exam.Api/Controllers/TiKuController.cs
+++ src/ExamEngineService/Exam.Api/Controllers/TiKuController.cs
@@ -7,6 +7,10 @@ using Exam.Model.QueryFilters;
 using Exam.Service.Interface;
 using DCommon.LinqToNPOI;
 using DCommon.LinqToNPOI.Writer;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace Exam.Api.Controllers
 {
@@ -22,6 +26,22 @@ namespace Exam.Api.Controllers
         {
             tiKuService.CreateTiKu(model);
             return ApiOk();
+        }
+
+        [HttpGet]
+        public HttpResponseMessage ExportProcessInfo([FromUri] string processName)
+        {
+            var path = @"D:\400中心_数据库文档.xls";
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+            var stream = new FileStream(path, FileMode.Open);
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentType =
+                new MediaTypeHeaderValue("application/octet-stream");
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "400中心_数据库文档.xls"
+            };
+            return result;
         }
 
         [HttpPost]
